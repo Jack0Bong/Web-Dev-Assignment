567af415f9d15430b366e1ec83e4a89bc5ee862a
JackBong <bonghjack@gmail.com>
2021-09-18 00:20:40 +0800

    added brian content

--- Js/shops.js
+++
@@ -1,3 +0,0 @@
-let jsonFile = new XMLHttpRequest();
-
-jsonFile.open("GET","http://127.0.0.1:5500/");
--- Js/tab.js
+++ Js/tab.js
@@ -1,11 +1,18 @@
-// Author:Bong Hung Jiat
-
+// Author: Brian Lai Kah Hoe
 tabHandler(0);
 
 function tabHandler(tabindex){
     let tab = document.querySelectorAll(".card-view");
     for(let i =0;i < tab.length;i++){
         tab[i].style.display="none";
+
     }
     tab[tabindex].style.display="grid";
 }
+
+$('button.tablink').on('click', function() {
+    var activeLink = $('.active');
+    activeLink.removeClass('active'); 
+    $(this).parent().addClass('active');
+});
+
